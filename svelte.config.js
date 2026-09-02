596eee940774da5e0682f6c616dff07804dd26d3
Sujay <sujayypatil08@gmail.com>
2026-09-02 10:22:20 +0530

    Fix GitHub Pages Routing

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,18 @@
+// import adapter from '@sveltejs/adapter-static';
+
+// const config = {
+// 	kit: {
+// 		paths: {
+// 			base: '/Attendion'
+// 		},
+// 		adapter: adapter({
+// 			fallback: 'index.html'
+// 		})
+// 	}
+// };
+
+// export default config;
+
 import adapter from '@sveltejs/adapter-static';
 
 const config = {
